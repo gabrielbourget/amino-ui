c80600a7b6789d7446647089220a9711a22fa13e
Gabriel Bourget <gabrielbourget@Gabriels-MacBook-Pro.local>
2024-04-07 18:51:17 -0400

    Feature: Turborepo setup

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+import turbo from "turbo";
+
+export default [
+  {
+    "extends": {
+      "next/core-web-vitals": coreWebVitals,
+      "turbo": turbo,
+    },
+  }
+];
--- packages/CLI/src/index.ts
+++ packages/CLI/src/index.ts
@@ -1,6 +1,6 @@
 import { Command } from "commander";
 // import figlet from "figlet";
-import { getPackageInfo } from "./utils";
+import { getPackageInfo } from "./utils/getPackageInfo";
 
 process.on("SIGINT", () => process.exit(0));
 process.on("SIGTERM", () => process.exit(0));
